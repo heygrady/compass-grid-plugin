25ef2d9ddd53113cc35a4de15462ad195663c6e1
Grady Kuhnline <github@heygrady.net>
2012-04-19 18:01:34 -0700

    fixing the tests to match api changes

--- test/config.rb
+++ test/config.rb
@@ -1,6 +1,6 @@
 # Require any additional compass plugins here.
 require 'compass-h5bp'
-Sass::Script::Number.precision = 8
+require 'compass-grid'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
