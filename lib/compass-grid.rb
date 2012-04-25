0422f796bdd7b547be38905d90b6dcc14caf46c2
Grady Kuhnline <github@heygrady.net>
2012-04-25 14:58:42 -0700

    initial tests on making the fluid percentages more precise with deep nesting by taking precision into account.

--- lib/compass-grid.rb
+++ lib/compass-grid.rb
@@ -3,3 +3,16 @@ require 'compass/grid'
 
 # Ensure maximum useful precision
 Sass::Script::Number.precision = 7
+
+module Sass::Script::Functions
+  def precision(*args)
+  	if !args[0].nil?
+  		precision = args[0]
+  		assert_type precision, :Number
+  		Sass::Script::Number.precision = precision.value
+  	end
+    Sass::Script::Number.new(Sass::Script::Number.precision)
+  end
+
+  declare :precision, :args => [:number]
+end
