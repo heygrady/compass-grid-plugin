fe3f6de9e66861e432e8cc76c5f601b533f46848
Grady Kuhnline <github@heygrady.net>
2012-04-30 16:23:57 -0700

    Adding support for IE7 to the fluid grid. Cleaning up some functions and mixins in grid and fluid for consistency.

--- lib/compass-grid.rb
+++ lib/compass-grid.rb
@@ -6,11 +6,11 @@ Sass::Script::Number.precision = 7
 
 module Sass::Script::Functions
   def precision(*args)
-  	if !args[0].nil?
-  		precision = args[0]
-  		assert_type precision, :Number
-  		Sass::Script::Number.precision = precision.value
-  	end
+    if !args[0].nil?
+      precision = args[0]
+      assert_type precision, :Number
+      Sass::Script::Number.precision = precision.value
+    end
     Sass::Script::Number.new(Sass::Script::Number.precision)
   end
 
